5124b3d09581ea99f1ec46d835bf9d38c937fcfa
Kevin Arehart <kevinarehart@gmail.com>
2017-03-27 19:36:26 -0500

    first load of changes

--- main.py
+++ main.py
@@ -1,24 +1,61 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import cgi
+
+def build_page(textarea_content):
+   username_label = "<label>Username</label>"
+   textarea_username = "<textarea name='username' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
+
+
+   password_label = "<label>Password</label>"
+   textarea_password = "<textarea name='password' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
+
+   verifyPassword_label = "<label>Verify Password</label>"
+   textarea_verifyPassword = "<textarea name='verifyPassword' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
+
+   email_label = "<label>Email (optional)</label>"
+   textarea_email = "<textarea name='email' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
+
+   submit = "<input type='submit'/>"
+   form = ("<form method='post'>" +
+       username_label + textarea_username + "<br>" +
+       password_label + textarea_password + "<br>" +
+       verifyPassword_label + textarea_verifyPassword + "<br>" +
+       email_label + textarea_email + "<br>" +
+       submit + "</form>")
+
+<body>
+    <h2>Enter some text to ROT13:</h2>
+    <form method="post">
+      <textarea name="text"
+                style="height: 100px; width: 400px;"></textarea>
+      <br>
+      <input type="submit">
+    </form>
+  </body>
+
+   header = "<h1>Signup</h1>"
+
+   return header + form
+
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+
+   def get(self):
+       content = build_page("")
+       self.response.write(content)
+
+   def post(self):
+       username = self.request.get("username")
+       password = self.request.get("password")
+       verifyPassword = self.request.get("verifyPassword")
+       email = self.request.get("email")
+
+
+       returned_messages = username + password + verifyPassword + email
+       escaped_messages = cgi.escape(returned_messages)
+       content = build_page(escaped_messages)
+       self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
