5b7086b7f2418797eb84aaa4da472cdff894a3d4
Kevin Arehart <kevinarehart@gmail.com>
2017-04-03 20:21:26 -0500

    Got a visual in the browser

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
 import webapp2
 import cgi
 
+
 def build_page(textarea_content):
    username_label = "<label>Username</label>"
-   textarea_username = "<textarea name='username' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
-
+   username_input = "<input type='text' name='username'>"
 
    password_label = "<label>Password</label>"
-   textarea_password = "<textarea name='password' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
+   password_input = "<input type='text' name='password'>"
 
    verifyPassword_label = "<label>Verify Password</label>"
-   textarea_verifyPassword = "<textarea name='verifyPassword' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
+   verifyPassword_input = "<input type='text' name='verifyPassword'>"
 
    email_label = "<label>Email (optional)</label>"
-   textarea_email = "<textarea name='email' style='height: 30; width: 100;'>" + textarea_content + "</textarea><br>"
+   email_input = "<input type='text' name='email'>"
 
    submit = "<input type='submit'/>"
    form = ("<form method='post'>" +
-       username_label + textarea_username + "<br>" +
-       password_label + textarea_password + "<br>" +
-       verifyPassword_label + textarea_verifyPassword + "<br>" +
-       email_label + textarea_email + "<br>" +
+       username_label + username_input + "<br>" +
+       password_label + password_input + "<br>" +
+       verifyPassword_label + verifyPassword_input + "<br>" +
+       email_label + email_input + "<br>" +
        submit + "</form>")
-
-<body>
-    <h2>Enter some text to ROT13:</h2>
-    <form method="post">
-      <textarea name="text"
-                style="height: 100px; width: 400px;"></textarea>
-      <br>
-      <input type="submit">
-    </form>
-  </body>
 
    header = "<h1>Signup</h1>"
 
@@ -50,9 +40,8 @@ class MainHandler(webapp2.RequestHandler):
        verifyPassword = self.request.get("verifyPassword")
        email = self.request.get("email")
 
-
        returned_messages = username + password + verifyPassword + email
-       escaped_messages = cgi.escape(returned_messages)
+       escaped_messages = cgi.escape(returned_messages, quote=True)
        content = build_page(escaped_messages)
        self.response.write(content)
 
